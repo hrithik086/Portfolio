940637fb9ea161cec469c4f8aad8bd1f4afd236c
hrithik <hrithik086@gmail.com>
2025-02-09 21:37:58 +0530

    Background, dp and Main Title added

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutMeComponent } from "./about-me/about-me.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbNavModule],
+  imports: [RouterOutlet, NgbNavModule, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
